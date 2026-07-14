f7df0b31631bd9c507de057ecd12c0608028c697
i3bel <376323052@qq.com>
2026-07-14 20:14:33 +0800

    新增柏林背景

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
+import { useRoute } from 'vitepress'
 import type { Theme } from 'vitepress'
 import HomePage from './components/HomePage.vue'
 import BackToTop from './components/BackToTop.vue'
 import Tabs from './components/Tabs.vue'
+import InteractiveAsciiBackground from './components/InteractiveAsciiBackground.vue'
 import './style.css'
 
 export default {
@@ -13,8 +15,15 @@ export default {
     app.component('Tabs', Tabs)
   },
   Layout() {
-    return h(DefaultTheme.Layout, null, {
-      'layout-bottom': () => h(BackToTop),
-    })
+    const route = useRoute()
+    const excluded = ['/manga', '/docx-editor']
+    const showBg = !excluded.some((p) => route.path.startsWith(p))
+
+    return h('div', null, [
+      showBg ? h(InteractiveAsciiBackground) : null,
+      h(DefaultTheme.Layout, null, {
+        'layout-bottom': () => h(BackToTop),
+      }),
+    ])
   },
 } satisfies Theme
